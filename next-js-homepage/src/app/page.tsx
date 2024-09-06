5b579ed981bffbf7afb61d8b8771435ccebed21e
BenSnaith <bsnaith.bs@icloud.com>
2024-09-06 22:47:12 +0100

    Initial creation of next.js project

---
+++ next-js-homepage/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col bg-zinc-900 min-w-96 min-h-64 text-center mt-64 items-center justify-center">
+        <p>Ben Snaith</p>
+        <p>Computer Science</p>
+      </div>
+    </div>
+  );
+}
